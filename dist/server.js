6a0d1146f4e204a84e1cd91116f53eba0371aff8
Luke Skywalker <yoseph16181@gmail.com>
2024-10-07 20:24:10 -0500

    Update app.js to index.js

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,8 @@
-import('../src/app.js')
+import('../src/index.js')
   .then((module) => {
-    const app = module.default; // Get the default export from app.js
+    const app = module.default; // Get the default export from index.js
     app(); // Call the app (main menu function)
   })
   .catch((err) => {
-    console.error('Error importing app.js:', err);
+    console.error('Error importing index.js:', err);
   });
